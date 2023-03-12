28aec7bcb09d318efb6ff594ad977c09ddc7f073
Roshan <90826638+pepesir@users.noreply.github.com>
2023-03-12 08:05:50 +0530

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,21 @@
+(async () => {
+ 
+  try{
+	switch (command) {
+		case `${p}`+'ping': {
+
+let { performance } = require('perf_hooks')
+    let old = performance.now()
+    await x.sendMessage(m.chat, { text : '_Checking ping..._' }, {quoted : false})
+              let neww = performance.now()
+    let speed = neww - old
+    y = Math.ceil(speed)
+        x.sendMessage(m.chat, { text : `_${y} ms_` }, {quoted : false})
+            
+       }
+   break
+  }
+    } catch (e) {
+      console.log(e)
+   }
+})()
