350f7f4e4cd41e37aa9c73aaebdca6e5fed63d5a
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-13 16:11:39 +0530

    Umfum🙂💀

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -12,7 +12,7 @@
                 ]
                 let buttonMessage = {
                     image: { url: anu.thumbnail },
-                    caption: `
+                    caption: `|`|`|`
 ᴛɪᴛʟᴇ : ${anu.title}
 ᴅᴜʀᴀᴛɪᴏɴ : ${anu.timestamp}
 ᴠɪᴇᴡᴇʀꜱ : ${anu.views}
@@ -20,7 +20,7 @@
 ᴀᴜᴛʜᴏʀ : ${anu.author.name}
 ᴄʜᴀɴɴᴇʟ : ${anu.author.url}
 ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ : ${anu.description}
-ᴠɪᴅᴇᴏ ᴜʀʟ : ${anu.url}`,
+ᴠɪᴅᴇᴏ ᴜʀʟ : ${anu.url}`|`|`|`,
                     footer: 'ᴡʜᴀᴛꜱ-ᴋʀɪᴢ-ᴀɪ',
                     buttons: buttons,
                     headerType: 4
@@ -28,8 +28,30 @@
                 x.sendMessage(m.chat, buttonMessage, { quoted: m })
             }
             break
+case `${p}`+'ytmp3': {
+if (!text) m.reply(`_Sᴇɴᴅ ʏᴏᴜᴛᴜʙᴇ ᴠɪᴅᴇᴏ ʟɪɴᴋ...!_`)
+let isLinks2 = args[0].match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)
+if (!isLinks2) return m.reply(`_Iɴᴠᴀʟɪᴅ ʟɪɴᴋ!_`)
+m.reply('_Yᴏᴜʀ ꜱᴏɴɢ ɪꜱ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...!_')
+anu = await ytMp4(`${q}`)
+titlenyaa = `|`|`|`ᴛɪᴛʟᴇ : ${anu.title}\nᴜᴘʟᴏᴀᴅ : ${anu.uploadDate}\nꜱɪᴢᴇ : ${anu.size}\nᴠɪᴇᴡꜱ : ${anu.views}\nʟɪᴋᴇ : ${anu.likes}\nᴅɪꜱʟɪᴋᴇ : ${anu.dislike}\nᴄʜᴀɴɴᴇʟ : ${anu.channel}\nᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ : ${anu.desc}`|`|`|`
+
+x.sendMessage(m.chat, { audio: { url: anu.result }, mimetype: 'audio/mpeg', fileName: `${anu.title}.mp3` }, { quoted: m })
+}
+break
+case `${p}`+'ytmp4': {
+if (!text) m.reply(`_Sᴇɴᴅ ʏᴏᴜᴛᴜʙᴇ ᴠɪᴅᴇᴏ ʟɪɴᴋ...!_`)
+let isLinks= args[0].match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)
+if (!isLinks) return m.reply(`_Iɴᴠᴀʟɪᴅ ʟɪɴᴋ!_)
+m.reply('_Yᴏᴜʀ ᴠɪᴅᴇᴏ ɪꜱ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...!_')
+anu = await ytMp4(`${q}`)
+titlenyaa = `|`|`|`ᴛɪᴛʟᴇ : ${anu.title}\nᴜᴘʟᴏᴀᴅ : ${anu.uploadDate}\nꜱɪᴢᴇ : ${anu.size}\nᴠɪᴇᴡꜱ : ${anu.views}\nʟɪᴋᴇ : ${anu.likes}\nᴅɪꜱʟɪᴋᴇ : ${anu.dislike}\nᴄʜᴀɴɴᴇʟ : ${anu.channel}\nᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ : ${anu.desc}`|`|`|`
+
+kriz.sendMessage(m.chat, { video: { url: anu.result }, mimetype: 'video/mp4', fileName: `${anu.title}.mp4` }, { quoted: m })
+}
+break
 case `${p}`+'play': {
-		if (!text) await m.reply('_${p} sᴏɴɢ xxxᴛᴇɴᴀᴄᴛɪᴏɴ ʜᴏᴘᴇ_')
+		if (!text) await m.reply('_sᴏɴɢ xxxᴛᴇɴᴀᴄᴛɪᴏɴ ʜᴏᴘᴇ_')
                 let search = await yts(text)
                 let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
                 let buttons = [
@@ -38,7 +60,7 @@ case `${p}`+'play': {
                 ]
                 let buttonMessage = {
                     image: { url: anu.thumbnail },
-                    caption: `
+                    caption: `|`|`|`
 ᴛɪᴛʟᴇ : ${anu.title}
 ᴅᴜʀᴀᴛɪᴏɴ : ${anu.timestamp}
 ᴠɪᴇᴡᴇʀꜱ : ${anu.views}
@@ -46,7 +68,7 @@ case `${p}`+'play': {
 ᴀᴜᴛʜᴏʀ : ${anu.author.name}
 ᴄʜᴀɴɴᴇʟ : ${anu.author.url}
 ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ : ${anu.description}
-ᴠɪᴅᴇᴏ ᴜʀʟ : ${anu.url}`,
+ᴠɪᴅᴇᴏ ᴜʀʟ : ${anu.url}`|`|`|`,
                     footer: 'ᴡʜᴀᴛꜱ-ᴋʀɪᴢ-ᴀɪ',
                     buttons: buttons,
                     headerType: 4
