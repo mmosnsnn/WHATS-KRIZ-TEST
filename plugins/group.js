ffac1a75b17cc6709bb64bf8c0d7a3eb25cec740
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-13 20:46:42 +0530

    Fisk🗿🍷

--- plugins/group.js
+++ plugins/group.js
@@ -4,11 +4,11 @@
 	switch (command) {
 	case `${p}`+'add': { 
 	    if (!m.isGroup) await m.reply('_This command is for groups!_')
-	    match = match || m.reply_m.jid;
+	    let jid = parsedJid(match)
+            match = match || m.reply_m.jid;
 	    if (!match) await m.reply('_Give a number to add!_')
         if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
         if (!isAdmins) await m.reply('_This command only for group admins!_')
-        let jid = parsedJid(match)
         let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
         await x.groupParticipantsUpdate(m.chat, [users], 'add')
         await m.reply(`_@${jid[0].split("@")[0]} added to this group!_`, { mentions: jid, })
