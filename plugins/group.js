7b20db56a44381f8e39198ba75e8c9b329b6f02b
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-14 21:22:05 +0530

    Fisk❗

--- plugins/group.js
+++ plugins/group.js
@@ -4,7 +4,6 @@
 	switch (command) {
 	case `${p}`+'add': { 
 	    if (!m.isGroup) await m.reply('_This command is for groups!_)
-            text = text || m.reply_m.chat;
 	    if (!text) await m.reply('_Give a number to add!_')
         if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
         if (!isAdmins) await m.reply('_This command only for group admins!_')
