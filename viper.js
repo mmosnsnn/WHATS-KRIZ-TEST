b09f1befba9bc3b71faa5b795b7647fd11227b0b
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-14 22:27:52 +0530

    Uhm

--- viper.js
+++ viper.js
@@ -40,7 +40,8 @@ try {
         const groupOwner = m.isGroup ? groupMetadata.owner : ''
         const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
         const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
-
+        var randomColor = ['#ef1a11', '#89cff0', '#660000', '#87a96b', '#e9f6ff', '#ffe7f7', '#ca86b0', '#83a3ee', '#abcc88', '#80bd76', '#6a84bd', '#5d8d7f', '#530101', '#863434', '#013337', '#133700', '#2f3641', '#cc4291', '#7c4848', '#8a496b', '#722f37', '#0fc163', '#2f3641', '#e7a6cb', '#64c987', '#e6e6fa', '#ffa500'];
+	const apiColor = randomColor[Math.floor(Math.random() * randomColor.length)];
 //autodl yt
 if (budy.startsWith("https://youtu")) {
 takes = budy.replace('https://youtube.com/shorts/','').replace('?feature=share','').replace('https://youtube.com/watch?v=','').replace('https://youtu.be/','')   
