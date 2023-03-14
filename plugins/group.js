ae2f33aca048e57303d18eeb8ce7007fd9895db0
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-14 21:16:42 +0530

    Fisk

--- plugins/group.js
+++ plugins/group.js
@@ -9,7 +9,7 @@
         if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
         if (!isAdmins) await m.reply('_This command only for group admins!_')
         let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-        let jid = parsedJid(match)
+        let jid = parsedJid(text)
         await x.groupParticipantsUpdate(m.chat, [users], 'add')
         await m.reply(`_@${jid[0].split("@")[0]} added to this group!_`, { mentions: jid, })
 }
