9d4427339dbd5eb621e79e5f95457e7e6a235262
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-14 10:17:32 +0530

    Uhm🗿🍷

--- plugins/converter.js
+++ plugins/converter.js
@@ -8,7 +8,7 @@
             if (!quoted) m.reply(`_Reply to audio or video..!_`)
             let media = await quoted.download()
             let audio = await toAudio(media, 'mp4')
-            x.sendMessage(m.chat, {audio: audio, mimetype: 'audio/mpeg', fileName: `ᴡʜᴀᴛꜱ-ᴋʀɪᴢ-ᴀɪ.mp3`, { quoted: m})                 
+            x.sendMessage(m.chat, {audio: audio, mimetype: 'audio/mpeg', fileName: `ᴡʜᴀᴛꜱ-ᴋʀɪᴢ-ᴀɪ.mp3`}, { quoted: m})                 
             }
             break
 }
