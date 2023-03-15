2f26dfd57287956e927fb58d8883023b6458f939
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-15 17:38:13 +0530

    uhm added gjid

--- plugins/misc.js
+++ plugins/misc.js
@@ -12,6 +12,28 @@
     x.sendContact(m.chat, global.owner, m)
     }
     break
+    case `${p}`+'getjids': {
+      if (!isCreator) await m.reply('_Only for owner!_')
+            let getGroups = await x.groupFetchAllParticipating();
+            let groups = Object.entries(getGroups)
+                .slice(0)
+                .map((entry) => entry[1]);
+            let anu = groups.map((v) => v.id);
+            let gjidmsg = `ᴀʟʟ ɢʀᴏᴜᴘꜱ ᴊɪᴅ\n\n`
+            for (let i of anu) {
+                let metadata = await x.groupMetadata(i);
+                await sleep(500)
+                gjidmsg += `ꜱᴜʙᴊᴇᴄᴛ:- ${metadata.subject}\n`
+                gjidmsg += `ᴍᴇᴍʙᴇʀ:- ${metadata.participants.length}\n`
+                gjidmsg += `ᴊɪᴅ:- ${i}\n\n`
+
+            }
+            m.reply(gjidmsg)
+
+        }
+    
+    
+    break
      
   }
     } catch (e) {
