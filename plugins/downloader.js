336d9d908f3cc4cb7b1d8e12d4ef7d9802d0e3df
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-15 14:55:47 +0530

    Uhm

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -74,7 +74,7 @@ x.sendMessage(m.chat, { video: { url: anu.result }, mimetype: 'video/mp4', fileN
 break
 case `${p}`+'insta': {
 if (!text) await m.reply('_Need insta url!_')
-const response = await fetchJson(`https://api-viper-x0.up.railway.app/api/insta?url=${text}`)
+const response = insta(text)
 x.sendMessage(m.chat, { video: { url: response.media.url }, fileName: 'ig.mp4', mimetype: 'video/mp4' }, { quoted: m })
 }
 break
