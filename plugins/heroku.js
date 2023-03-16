79e19d51b35cabf6481a3b205c67f1fbcb6eb17b
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-16 18:57:17 +0530

    Hm

---
+++ plugins/heroku.js
@@ -0,0 +1,59 @@
+const Heroku = require("heroku-client")
+const got = require("got")
+const simpleGit = require("simple-git")
+const git = new simpleGit()
+const heroku = new Heroku({ token: config.HEROKU_API_KEY });
+const baseURI = "/apps/" + config.HEROKU_APP_NAME;
+const exec = require("child_process").exec;
+
+(async () => {
+ 
+  try{
+	switch (command) {
+	case `${p}`+'restart': {
+	await x.sendMessage(`_Restarting...!_`)
+	await heroku.delete(baseURI + "/dynos").catch(async (error) => {
+	await x.sendMessage(`HEROKU : ${error.body.m}`)
+	})
+	}
+	break
+    case `${p}`+'shutdown': {
+    await heroku
+    .get(baseURI + "/formation")
+    .then(async (formation) => {
+    await x.sendMessage(`_Shutting down...!_`)
+    await heroku.patch(baseURI + "/formation/" + formation[0].id, {
+    body: {
+      quantity: 0,
+    },
+    })
+    })
+    .catch(async (error) => {
+    await x.sendMessage(`HEROKU : ${error.body.m}`)
+    })
+    }
+    break
+   case `${p}`+'setvar': {
+   if (!text) await m.reply(`_Example : .setvar BOT_NAME:WHATS-KRIZ-AI_`)
+   const [key, value] = text.split(":")
+   if (!key || !value)
+   return await m.reply(`_Example : .setvar BOT_NAME:WHATS-KRIZ-AI_`)
+   heroku
+   .patch(baseURI + "/config-vars", {
+   body: {
+     [key.toUppercase()]: value,
+     },
+     })
+     .then(async () => {
+     await x.sendMessage(`_${key.toUpperCase()}: ${value}_`)
+     })
+     .catch (async (error) => {
+     await x.sendMessage(`HEROKU : ${error.body.m}`)
+     })
+     }
+    break
+    }
+    } catch (e) {
+      console.log(e)
+   }
+})()
