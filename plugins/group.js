0fae7cc420e5c39bf85c54f2b8f17d96e0794696
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-16 21:00:56 +0530

    Hm

--- plugins/group.js
+++ plugins/group.js
@@ -6,7 +6,7 @@
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
 		let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-		await kriz.groupParticipantsUpdate(m.chat, [users], 'add')
+		await x.groupParticipantsUpdate(m.chat, [users], 'add')
         await m.reply(`_@${users.split('@')[0]} added to this group!_`)
 	}
 	break
