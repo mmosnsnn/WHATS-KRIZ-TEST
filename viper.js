02fb8c5618cb66e6f9fdabdf7dd67b0759f23001
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-16 21:57:20 +0530

    Hm

--- viper.js
+++ viper.js
@@ -12,7 +12,7 @@ const { insta } = require('./lib/scrapers.js')
 const { ytMp4, ytMp3, ytPlay } = require('./lib/ytdl.js')
 const { toAudio } = require('./lib/converter.js')
 const yts = require('yt-search')
-const { parsedJid, fetchJson, sleep } = require('./lib/myfunc.js')
+const { parsedJid, fetchJson, sleep, fetchBuffer } = require('./lib/myfunc.js')
 
 module.exports = viper = async (x, m, chatUpdate) => {
 try {
