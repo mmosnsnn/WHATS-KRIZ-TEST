b82e8835735876ca22f3fa732bbf9d8fd9e6dc31
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-16 21:34:31 +0530

    Hm

--- plugins/group.js
+++ plugins/group.js
@@ -13,12 +13,12 @@
 	}
 	break
 case `${p}`+'promote': {
-		                if (!text) await m.reply(`_Give a number to add!_`)
+		                if (!text) await m.reply(`_Give a user to promote!_`)
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
 		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 		let jid = parsedJid(users)
-		await kriz.groupParticipantsUpdate(m.chat, [users], 'promote')
+		await x.groupParticipantsUpdate(m.chat, [users], 'promote')
         await m.reply(`_@${jid.split('@')[0]} promoted as admin!_`)
 	}
 	break
