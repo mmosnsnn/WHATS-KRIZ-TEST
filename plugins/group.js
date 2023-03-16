d9b8b763de53c71c3591236bd63d8b4d2f9113ad
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-16 21:19:45 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -3,11 +3,13 @@
   try{
 	switch (command) {
 	case `${p}`+'add': {
+                if (!text) await m.reply(`_Give a number to add!_`)
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
+                let jid = parsedJid(text)
 		let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 		await x.groupParticipantsUpdate(m.chat, [users], 'add')
-        await m.reply(`_@${users.split('@')[0]} added to this group!_`)
+        await m.reply(`_@${jid.split('@')[0]} added to this group!_`)
 	}
 	break
 }
