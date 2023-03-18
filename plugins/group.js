5b92730069fdbe60e5cc77a06f69ac11b5ee97d8
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-18 11:51:20 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -7,9 +7,8 @@
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
 		let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-		let jid = parsedJid(users)
 		await x.groupParticipantsUpdate(m.chat, [users], 'add')
-        await m.reply(`_@${jid.split('@')[0]} added to this group!_`)
+        await m.reply(`_@${users.split('@')[0]} added to this group!_`)
 	}
 	break
 case `${p}`+'promote': {
@@ -17,9 +16,8 @@ case `${p}`+'promote': {
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
 		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-		let jid = parsedJid(users)
 		await x.groupParticipantsUpdate(m.chat, [users], 'promote')
-        await m.reply(`_@${jid.split('@')[0]} promoted as admin!_`)
+        await m.reply(`_@${users.split('@')[0]} promoted as admin!_`)
 	}
 	break
 }
