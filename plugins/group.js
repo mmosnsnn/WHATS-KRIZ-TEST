635ba89722d4803d9025f7a81c244e7b138e7159
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-18 11:53:04 +0530

    Hm

--- plugins/group.js
+++ plugins/group.js
@@ -3,7 +3,7 @@
   try{
 	switch (command) {
 	case `${p}`+'add': {
-                if (!text) await m.reply(`_Give a number to add!_`)
+                if (!teks) await m.reply(`_Give a number to add!_`)
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
 		let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
@@ -12,7 +12,7 @@
 	}
 	break
 case `${p}`+'promote': {
-		                if (!text) await m.reply(`_Give a user to promote!_`)
+		                if (!teks) await m.reply(`_Give a user to promote!_`)
 		if (!m.isGroup) await m.reply(`_This command is only for use in groups!_`)
                 if (!isBotAdmins) await m.reply(`_I'm not admin!_`)
 		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
