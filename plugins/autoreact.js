00b912fb12f8e0ca90dcbad06eee537c19762fcc
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-20 08:02:49 +0530

    Hem

--- plugins/autoreact.js
+++ plugins/autoreact.js
@@ -5,12 +5,12 @@
 	case `{p}`+'autoreact': {
 	if (!isCreator) await m.reply(`_This feature only owner can enable!._`)
 	if (args[0] === "on") {
-	if (db.data.chats[m.chat].autoreact) return m.reply(`_Autoreact is already enabled!_`)
-	db.data.chats[m.chat].autoreact = true
+	if (db.data.chats[m.chat].react) return m.reply(`_Autoreact is already enabled!_`)
+	db.data.chats[m.chat].react = true
     m.reply(`_Autoreact is enabled!_`)
     } else if (args[0] === "off") {
-    if (!db.data.chats[m.chat].autoreact) return m.reply(`_Autoreact is already enabled!_`)
-    db.data.chats[m.chat].autoreact = false
+    if (!db.data.chats[m.chat].react) return m.reply(`_Autoreact is already enabled!_`)
+    db.data.chats[m.chat].react = false
     m.reply(`_Autoreact is disabled!_`)
     } else {
     const buttons = [
