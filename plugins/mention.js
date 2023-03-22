bc6277962e48532ea594bd9838c8af4f493d1f82
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-22 23:34:27 +0530

    Hem

---
+++ plugins/mention.js
@@ -0,0 +1,40 @@
+(async () => {
+ 
+  try{
+	switch (command) {
+case `${p}`+'mention': {
+let { result } = await getJson(`https://gist.githubusercontent.com/Djjdkdm/03c3119faf9151cb5add5bbdd1f21f1a/raw/5933860ed07feb39dfbe65cfdc27ed4d5c29b069/mention.json`)
+var logo = 'https://i.imgur.com/LgwJjMF.jpeg'
+for (any in owner)
+if (text.includes(owner[any])) {
+const audio = result[Math.floor(Math.random() * result.length)]
+let image1 = await getBuffer(logo)
+let image2 = await getBuffer(logo)
+x.sendMessage(m.chat, {
+audio: { url: audio },
+mimetype: 'audio/mpeg',
+ptt: true,
+waveform: [99,50,99,50,99],
+contextInfo: {
+				externalAdReply: {
+				title: 'WʜᴀᴛꜱKʀɪᴢ Aɪ',
+				body: 'ᴛᴏxɪᴄ-ᴋɪᴄʜᴜx🥶!!',
+				mediaType: 2,
+				thumbnail: image2,
+				mediaUrl: 'https://www.instagram.com/t.o.x.i.c_k.i.c.h.u',
+				sourceUrl: 'https://www.instagram.com/t.o.x.i.c_k.i.c.h.u',
+				showAdAttribution: true
+                }}
+                },
+                { quoted: m})
+	
+		    } catch (e) {
+			    x.sendMessage(m.chat , { text : "(☞ ͡° ͜ʖ ͡°)☞     " + e } )
+			    }
+}
+break
+}
+    } catch (e) {
+      console.log(e)
+   }
+})()
