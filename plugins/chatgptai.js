06d5a41fa7bed97647da2243d5e8bafa9e979285
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-03-23 12:02:59 +0530

    Hem

--- plugins/chatgptai.js
+++ plugins/chatgptai.js
@@ -4,7 +4,7 @@
 	switch (command) {
 	case `${p}`+'ai': {
 	if (!teks) await m.reply(`_Example : .ai who is what's kriz?_`)
-    let response = await axios (`https://api-viper-x0.vercel.app/api/openai?openaiapikey=${gptapikey}&text=${teks}`)
+    let response = await axios(`https://api-viper-x0.vercel.app/api/openai?openaiapikey=${gptapikey}&text=${teks}`)
     x.sendMessage(m.chat, { text: response.data.text }, {quoted: m })
 }
 break
