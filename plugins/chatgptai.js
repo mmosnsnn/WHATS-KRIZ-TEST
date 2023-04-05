7648a8572d21b8765e2cf4047d5f5a737f252d79
Sandun-Mihiranga2 <102826338+Sandun-Mihiranga2@users.noreply.github.com>
2023-04-05 17:02:43 +0530

    Update chatgptai.js

--- plugins/chatgptai.js
+++ plugins/chatgptai.js
@@ -1,15 +1,23 @@
-(async () => {
- 
-  try{
-	switch (command) {
-	case `${p}`+'ai': {
-	if (!teks) await m.reply(`_Example : .ai who is what's kriz?_`)
-    let response = await fetchJson(`https://api-viper-x0.vercel.app/api/openai?openaiapikey=${gptapikey}&text=${teks}`)
-    x.sendMessage(m.chat, { text: response.data.text }, {quoted: m })
-}
-break
-}
-    } catch (e) {
-      console.log(e)
-   }
-})()
+const config = require('../config')
+const { bot, getGPTResponse } = require('../lib')
+
+bot(
+	{
+		pattern: 'gpt ?(.*)',
+		fromMe: true,
+		desc: 'ChatGPT fun',
+		type: 'AI',
+	},
+	async (message, match) => {
+		if (!config.GPT)
+			return await message.send(
+				'Get a key from https://platform.openai.com/account/api-keys\n\nsetvar GPT = your_key'
+			)
+		if (!match)
+			return await message.send(
+				'*Example : gpt What is the capital of France?*'
+			)
+		const res = await getGPTResponse(match)
+		await message.send(res, { quoted: message.data })
+	}
+)
