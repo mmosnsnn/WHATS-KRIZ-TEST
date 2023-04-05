4ff0ab43e4bd7e3daaeea95dc61992df93963ebf
Sandun-Mihiranga2 <102826338+Sandun-Mihiranga2@users.noreply.github.com>
2023-04-05 22:05:29 +0530

    Fix

---
+++ plugins/chatgptai.js
@@ -0,0 +1,15 @@
+(async () => {
+ 
+  try{
+	switch (command) {
+	case `${p}`+'ai': {
+	if (!teks) await m.reply(`_Example : .ai who is what's kriz?_`)
+    let response = await fetchJson(`https://api-viper-x0.vercel.app/api/openai?openaiapikey=${gptapikey}&text=${teks}`)
+    x.sendMessage(m.chat, { text: response.data.text }, {quoted: m })
+}
+break
+}
+    } catch (e) {
+      console.log(e)
+   }
+})()
